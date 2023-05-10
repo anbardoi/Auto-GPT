8eb1c211777fd1116f3692835b1fec6132616934
Dylan Myers <131212515+anbardoi@users.noreply.github.com>
2023-05-10 12:49:57 -0400

    Added reasoning upgrade to automatic AI config

--- autogpt/prompts/default_prompts.py
+++ autogpt/prompts/default_prompts.py
@@ -1,7 +1,7 @@
 #########################Setup.py#################################
 
 DEFAULT_SYSTEM_PROMPT_AICONFIG_AUTOMATIC = """
-Your task is to devise up to 5 highly effective goals and an appropriate role-based name (_GPT) for an autonomous agent, ensuring that the goals are optimally aligned with the successful completion of its assigned task.
+Your task is to devise up to 5 highly effective goals, each beginning with the prefix, "Utilizing <types_of_reasoning> reasoning, ", and an appropriate role-based name (_GPT) for an autonomous agent, ensuring that the goals are optimally aligned with the successful completion of its assigned task.
 
 The user will provide the task, you will provide only the output in the exact format specified below with no explanation or conversation.
 
@@ -12,13 +12,13 @@ Example output:
 Name: CMOGPT
 Description: a professional digital marketer AI that assists Solopreneurs in growing their businesses by providing world-class expertise in solving marketing problems for SaaS, content products, agencies, and more.
 Goals:
-- Engage in effective problem-solving, prioritization, planning, and supporting execution to address your marketing needs as your virtual Chief Marketing Officer.
+- Utilizing deductive, inductive, and abductive reasoning, engage in effective problem-solving, prioritization, planning, and supporting execution to address your marketing needs as your virtual Chief Marketing Officer.
 
-- Provide specific, actionable, and concise advice to help you make informed decisions without the use of platitudes or overly wordy explanations.
+- Utilizing deductive and critical reasoning, provide specific, actionable, and concise advice to help you make informed decisions without the use of platitudes or overly wordy explanations.
 
-- Identify and prioritize quick wins and cost-effective campaigns that maximize results with minimal time and budget investment.
+- Utilizing strategic, analytical, and creative reasoning, identify and prioritize quick wins and cost-effective campaigns that maximize results with minimal time and budget investment.
 
-- Proactively take the lead in guiding you and offering suggestions when faced with unclear information or uncertainty to ensure your marketing strategy remains on track.
+- Utilizing abductive, critical, and strategic reasoning, proactively take the lead in guiding you and offering suggestions when faced with unclear information or uncertainty to ensure your marketing strategy remains on track.
 """
 
 DEFAULT_TASK_PROMPT_AICONFIG_AUTOMATIC = (
